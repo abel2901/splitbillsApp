14c1aec732404ec2e0b1fb98dc754fc85ddd929d
Abel Silva <abel2901@gmail.com>
2025-09-07 15:55:30 -0300

    Adiciona docker file

--- Program.cs
+++ Program.cs
@@ -2,20 +2,41 @@ using Microsoft.EntityFrameworkCore;
 using SplitBillsApi.Data;
 using SplitBillsApi.Hubs;
 using SplitBillsApi.Service;
+using System.Runtime.Intrinsics.X86;
+using System.Text.Json.Serialization;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// --- Configurar porta dinâmica (Railway) ---
+var port = Environment.GetEnvironmentVariable("PORT") ?? "5000";
+builder.WebHost.UseUrls($"http://*:{port}");
 
 // Add services to the container.
 builder.Services.AddDbContext<AppDbContext>(opt =>
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddScoped<SaldoService>();
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(opt =>
+    {
+        opt.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles;
+    }); ;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddSignalR();
+
+// --- CORS ---
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
 
 var app = builder.Build();
 
@@ -24,12 +45,22 @@ app.MapHub<GastosHub>("/gastosHub");
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
+    app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI();
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "SplitBills API V1");
+    });
+}
+else
+{
+    // Configurações para produção
+    app.UseExceptionHandler("/error"); // Página ou endpoint de erro
+    app.UseHsts(); // Segurança HTTPS
 }
 
 app.UseHttpsRedirection();
-
+app.UseCors("AllowAll");
 app.UseAuthorization();
 
 
--- obj/Debug/net6.0/SplitBillsApi.AssemblyInfo.cs
+++ obj/Debug/net6.0/SplitBillsApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SplitBillsApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+40f1c3390dfd4e6727589761364b8b61da975470")]
 [assembly: System.Reflection.AssemblyProductAttribute("SplitBillsApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SplitBillsApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
